e9381daae6baacc8a23b85c7fff01ec59429d143
Charonus2012 <Charonus2012@users.noreply.github.com>
2025-08-09 23:33:01 +0200

    Update of auth backend and frontend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 import mariadb
-from fastapi import FastAPI
+import hashlib
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
+from starlette.middleware.cors import CORSMiddleware
+
 from dbh import create_connection
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Sem můžeš dát třeba ["http://localhost:5500"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode("utf-8")).hexdigest()
+
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     html_content = """
@@ -20,7 +35,10 @@ def read_root():
     return html_content
 
 @app.post("/login")
-def login(username: str, pwd: str):
+def login(
+username: str = Form(...),
+password: str = Form(...)
+):
     conn = create_connection()
     if conn:
         cur = conn.cursor()
@@ -30,35 +48,56 @@ def login(username: str, pwd: str):
                 for row in cur:
                     upwd = row[0]
             except:
-                return {"message": "bad"}
-                upwd = 0
+                return {"type": "err", "msg": "Špatné uživatelské jméno nebo heslo"}
             cur.close()
             conn.close()
-            if pwd == upwd:
-                return {"message": "success"}
+            if hash_password(password) == upwd:
+                return {"type": "scs", "msg": "success"}
             else:
-                return {"message": "bad"}
+                return {"type": "err", "msg": "Špatné uživatelské jméno nebo heslo"}
         except mariadb.Error as e:
             cur.close()
             conn.close()
-            return {"message": "err"}
-    return {"message": "dbconnerr"}
+            return {"type": "err", "msg": "Nevim co se stalo"}
+    return {"type": "err", "msg": "Chyba připojení do databáze"}
 
 
 @app.post("/signup")
-def signup(username: str, email: str, pwd: str):
+def signup(
+        username: str = Form(...),
+        email: str = Form(...),
+        password: str = Form(...),
+        confirm_password: str = Form(...)
+):
+    if password != confirm_password:
+        return {"type": "err", "msg": "Hesla se neshodují"}
     conn = create_connection()
     if conn:
         cur = conn.cursor()
         try:
-            cur.execute("INSERT INTO users SET username = ?, email = ?, pwd = ?", (username, email, pwd,))
+            cur.execute("SELECT username FROM users WHERE username = ?", (username,))
+            i = 0
+            for row in cur:
+                i += 1
+            if i != 0:
+                return {"type": "err", "msg": "Uživatelské jméno je používané"}
+            cur.execute("SELECT email FROM users WHERE email = ?", (email,))
+            i = 0
+            for row in cur:
+                i += 1
+            if i != 0:
+                return {"type": "err", "msg": "Email je už používaný"}
+        except mariadb.Error as e:
+            return {"type": "err", "msg": "Nevim co se stalo"}
+        try:
+            cur.execute("INSERT INTO users SET username = ?, email = ?, pwd = ?", (username, email, hash_password(password),))
             conn.commit()
         except mariadb.Error as e:
             cur.close()
             conn.close()
-            return {"message": "err"}
+            return {"type": "err", "msg": "Nevim co se stalo"}
         cur.close()
         conn.close()
-        return {"message": "success"}
-    return {"message": "dbconnerr"}
+        return {"type": "scs", "msg": "Podařilo se, můžete se přihlásit"}
+    return {"type": "err", "msg": "Chyba připojení do databáze"}
 
---
+++ web/auth/auth.js
@@ -0,0 +1,38 @@
+document.getElementById("signupForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    const response = await fetch("http://127.0.0.1:8000/signup", {
+        method: "POST",
+        body: formData
+    });
+
+    const result = await response.json();
+
+    const h2 = document.getElementById("err_signup");
+    h2.setAttribute("class", result.type);
+    if (result.type === "scs") {
+        document.querySelectorAll(".signlog").forEach(el => {
+            el.value = "";
+        });
+    }
+    h2.textContent = result.msg;
+});
+
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    const response = await fetch("http://127.0.0.1:8000/login", {
+        method: "POST",
+        body: formData
+    });
+
+    const result = await response.json();
+
+    const h2 = document.getElementById("err_login");
+    h2.setAttribute("class", result.type);
+    h2.textContent = result.msg;
+});
